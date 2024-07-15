c184c1584f124087085d523cf38bef160c6b01dc
sseung <sseutta@naver.com>
2024-07-15 19:34:47 +0900

    feat: TapPage
    바텀네비게이션 항목추가

--- lib/tap/tap_page.dart
+++ lib/tap/tap_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:untitled9/tap/account/account_page.dart';
+import 'package:untitled9/tap/home/home_page.dart';
+import 'package:untitled9/tap/ranking/raking_page.dart';
+import 'package:untitled9/tap/search/search_page.dart';
+import 'package:untitled9/tap/writing/writing_page.dart';
 
 class TapPage extends StatefulWidget {
   const TapPage({super.key});
@@ -9,17 +14,27 @@ class TapPage extends StatefulWidget {
 
 class _TapPageState extends State<TapPage> {
   int _currentIndex = 0;
+  final _pages = const [
+    HomePage(),
+    SearchPage(),
+    WritingPage(),
+    RankingPage(),
+    AccountPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         currentIndex: _currentIndex,
-        onTap: (index){
+        onTap: (index) {
+          print('잘되가나');
           setState(() {
             _currentIndex = index;
-          });
 
+          });
         },
         items: const [
           BottomNavigationBarItem(
@@ -29,6 +44,14 @@ class _TapPageState extends State<TapPage> {
           BottomNavigationBarItem(
             icon: Icon(Icons.search),
             label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.wallet),
+            label: 'Writing',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.perm_media_rounded),
+            label: 'Ranking',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
