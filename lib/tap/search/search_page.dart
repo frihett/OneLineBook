e8e30997975a9fd6509791954ac6bfc1ebddb799
sseung <sseutta@naver.com>
2024-07-15 19:52:58 +0900

    feat: SearchPage UI
    도서 검색 페이지 작성
    리스트뷰

---
+++ lib/tap/search/search_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+final List<String> entries = <String>['A', 'B', 'C'];
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('검색하기'),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    width: 300,
+                    child: TextFormField(
+                      onTap: () {},
+                      decoration: InputDecoration(
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xffD7D7D7),
+                            width: 3,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                        contentPadding:
+                            const EdgeInsets.symmetric(vertical: 8),
+                        hintText: '도서 검색하기',
+                        prefixIcon: const Padding(
+                          padding: EdgeInsets.only(left: 16.0),
+                          child: Icon(Icons.search),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+                Expanded(
+                  child: ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      itemCount: entries.length,
+                      itemBuilder: (BuildContext context, int index) {
+                            return Container(
+                              height: 50,
+                            
+                              child: Center(child: Text('Entry ${entries[index]}')),
+                            );
+                      }
+                  ),
+                )
+            ],
+          
+          
+          
+          ),
+        ));
+  }
+}
