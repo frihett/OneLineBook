7fe0a812dde3f6468d3f6f53d6ff4768d3e3f5ef
sseung <sseutta@naver.com>
2024-07-18 23:22:19 +0900

    feat: 데이터소스 클래스작성
    getBook 메소드

---
+++ lib/data_source/book_api.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:untitled9/dto/BookInfoDto.dart';
+import 'package:untitled9/mapper/book_mapper.dart';
+
+import '../model/book.dart';
+
+class BookApi {
+  Future<List<Book>> getBook(String query) async {
+    var url = 'https://dapi.kakao.com/v3/search/book';
+    var apiKey = '1934bda87c8f2d04588a246af4df2040';
+
+    var response = await http.get(
+      Uri.parse('$url?query=$query'),
+      headers: {
+        'Authorization': 'KakaoAK $apiKey',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final map = jsonDecode(response.body);
+
+      List<dynamic> bookInfoJson =
+          map['documents'] as List; // documents를 벗겨내고 list 로 받기
+
+      Iterable<BookInfoDto> bookInfoJson2 = bookInfoJson
+          .map((e) => BookInfoDto.fromJson(e)); // 리스트 요소들을 BookInfoDto 객체로 변환하기
+      return bookInfoJson2
+          .map((e) => e.toBook())
+          .toList(); // BookInfoDto List map to Book List
+    } else {
+      throw Exception('Failed to load book');
+    }
+  }
+}
