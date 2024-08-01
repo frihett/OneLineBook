cba32e00c0736a8ebea1c7d009c18faea0e7b9a2
sseung <sseutta@naver.com>
2024-08-01 16:18:48 +0900

    feat: 코드수정
    TapPage를 Shellroute의 상위에 넣기

--- lib/config/router/router.dart
+++ lib/config/router/router.dart
@@ -2,11 +2,13 @@ import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:untitled9/data/repository/book_repository_impl.dart';
 import 'package:untitled9/presentation/tap/account/account_page.dart';
+import 'package:untitled9/presentation/tap/account/account_page_view_model.dart';
 import 'package:untitled9/presentation/tap/home/home_page.dart';
 import 'package:untitled9/presentation/tap/home/home_page_view_model.dart';
 import 'package:untitled9/presentation/tap/login/login_page.dart';
 import 'package:untitled9/presentation/tap/ranking/raking_page.dart';
 import 'package:untitled9/presentation/tap/review_search/review_page.dart';
+import 'package:untitled9/presentation/tap/review_search/review_page_view_model.dart';
 import 'package:untitled9/presentation/tap/search/search_page.dart';
 import 'package:untitled9/presentation/tap/search/search_page_view_model.dart';
 import 'package:untitled9/presentation/tap/splash/splash_page.dart';
@@ -24,28 +26,40 @@ final goRouter = GoRouter(
         builder: (context, state) {
           return SplashScreen();
         }),
-    GoRoute(
-        path: '/tapPage',
-        builder: (context, state) {
-          return TapPage();
-        }),
-    GoRoute(
-        path: '/account',
-        builder: (context, state) {
-          return AccountPage();
-        }),
-    GoRoute(
-        path: '/home',
-        builder: (context, state) {
-          return HomePage(
-              homePageViewModel:
-                  HomePageViewModel(bookRepository: BookRepositoryImpl()));
-        }),
-    GoRoute(
-        path: '/reviewSearching',
-        builder: (context, state) {
-          return ReviewPage();
-        }),
+    ShellRoute(
+      builder: (context, state, child) {
+        return TapPage(child: child);
+      },
+      routes: [
+        GoRoute(
+            path: '/home',
+            builder: (context, state) {
+              return HomePage(
+                  homePageViewModel:
+                      HomePageViewModel(bookRepository: BookRepositoryImpl()));
+            }),
+        GoRoute(
+            path: '/reviewSearching',
+            builder: (context, state) {
+              return ChangeNotifierProvider(
+                  create: (context) => ReviewPageViewModel(),
+                  child: ReviewPage());
+            }),
+        GoRoute(
+            path: '/raking',
+            builder: (context, state) {
+              return RankingPage();
+            }),
+        GoRoute(
+            path: '/account',
+            builder: (context, state) {
+              return ChangeNotifierProvider(
+                  create: (context) => AccountViewModel(),
+                  child: AccountPage());
+            }),
+      ],
+    ),
+
     GoRoute(
         path: '/bookSearching',
         builder: (context, state) {
@@ -67,11 +81,7 @@ final goRouter = GoRouter(
                 book: book,
               ));
         }),
-    GoRoute(
-        path: '/raking',
-        builder: (context, state) {
-          return RankingPage();
-        }),
+
     GoRoute(
         path: '/login',
         builder: (context, state) {
