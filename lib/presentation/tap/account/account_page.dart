b8e25027bc6a6f01a53467e51d8eed86a10eff16
sseung <sseutta@naver.com>
2024-08-01 16:16:02 +0900

    feat: AccountPage 코드정리

--- lib/presentation/tap/account/account_page.dart
+++ lib/presentation/tap/account/account_page.dart
@@ -18,17 +18,11 @@ class _AccountPageState extends State<AccountPage> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(() {
-      final viewmodel = context.read<AccountViewModel>();
-      final viewmodel2 = context.read<UserProvider>();
-      viewmodel2.fetchUserInfo();
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final model = context.watch<AccountViewModel>();
-    final model2 = context.watch<UserProvider>();
+    final userProvider = context.watch<UserProvider>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('내 프로필'),
@@ -42,17 +36,17 @@ class _AccountPageState extends State<AccountPage> {
                 Container(
                   width: 100,
                   height: 100,
-                  child: model2.user?.photoURL != null
-                      ? Image.network(model2.user!.photoURL.toString())
+                  child: userProvider.user?.imageUrl != null
+                      ? Image.network(userProvider.user!.imageUrl.toString())
                       : Icon(Icons.account_circle, size: 100),
                 ),
                 Column(
                   children: [
-                    Text('안녕하세요 ${model2.user?.displayName}님'),
+                    Text('안녕하세요 ${userProvider.user?.name}님'),
                     SizedBox(
                       height: 12,
                     ),
-                    Text('이메일은 ${model2.user?.email ?? '알 수 없음'}')
+                    Text('이메일은 ${userProvider.user?.email ?? '알 수 없음'}')
                   ],
                 ),
               ],
--- lib/presentation/tap/account/account_page_view_model.dart
+++ lib/presentation/tap/account/account_page_view_model.dart
@@ -5,22 +5,9 @@ class AccountViewModel with ChangeNotifier {
   User? _user;
   bool _isLoading = true;
 
-  AccountViewModel() {
-
-  }
+  AccountViewModel() {}
 
   User? get user => _user;
-  bool get isLoading => _isLoading;
 
-  // Future<void> fetchUserInfo() async {
-  //   try {
-  //     _user = FirebaseAuth.instance.currentUser;
-  //   } catch (e) {
-  //     // Handle the error
-  //     print('Error fetching user info: $e');
-  //   } finally {
-  //     _isLoading = false;
-  //     notifyListeners();
-  //   }
-  // }
+  bool get isLoading => _isLoading;
 }
