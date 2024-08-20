a2d7ab05192f9b797f4d558bd8e7bc51d841852c
sseung <sseutta@naver.com>
2024-08-20 21:59:30 +0900

    feat: UerProvider 기능 삭제 및 추가
    setUser() - 유저변수에 초기화
    saveUser() - 파이어스토어에 User객체 저장
    fetchUser() - 유저 가져오기
    getUserId() - 유저 아이디 가져오기(전역에서 사용 가능 )

--- lib/core/provider/user_provider.dart
+++ lib/core/provider/user_provider.dart
@@ -1,23 +1,43 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/cupertino.dart';
+import 'package:untitled9/domain/use_case/create_user_use_case.dart';
 
-class UserProvider with ChangeNotifier {
+import '../../domain/model/user.dart';
+import '../../domain/use_case/get_user_use_case.dart';
+
+class UserProvider extends ChangeNotifier {
+  final GetUserUseCase _getUserUseCase;
+  final CreateUserUseCase _createUserUseCase;
+
+  UserProvider({
+    required GetUserUseCase getUserUseCase,
+    required CreateUserUseCase createUserUseCase,
+  })  : _getUserUseCase = getUserUseCase,
+        _createUserUseCase = createUserUseCase;
+
   User? _user;
 
   User? get user => _user;
 
-  Future<void> fetchUserInfo() async {
-    try {
-      _user = FirebaseAuth.instance.currentUser!;
-    } catch (e) {
-      // Handle the error
-      print('Error fetching user info: $e');
-    } finally {
-      notifyListeners();
-    }
+  void setUser(User user) {
+    _user = user;
+    notifyListeners(); // UI 업데이트를 위해 호출
   }
 
-  void logOut() async{
-    FirebaseAuth.instance.signOut();
+  void clearUser() {
+    _user = null;
+    notifyListeners(); // UI 업데이트를 위해 호출
+  }
+
+  Future<void> saveUser(User user) async {
+    await _createUserUseCase.execute(user: user);
+  }
+
+  Future<User?> fetchUser(String userId) async {
+    return _getUserUseCase.execute(userId: userId);
+  }
+
+  String getUserId() {
+    return auth.FirebaseAuth.instance.currentUser!.uid;
   }
 }
