1d3dfb25965ad6272903df50c07f77c20c0d7d5f
sseung <sseutta@naver.com>
2024-08-20 23:11:07 +0900

    feat: 여러가지 메소드 추가
    addReviewToUser() - 리뷰작성시 유저안에 리뷰 저장 하기
    deleteReviewToUser() - 리뷰삭제시 유저안에 리뷰 삭제 하기
    addCurrentReadingBookToUser() - 읽고 있는 책으로 저장하기 시 유저안에 북 객체 저장하기
    deleteCurrentReadingBookToUser() - 읽고 있는 책으로 저장해체시 유저안에 북 객체 삭제하기
    updateUsersReview() - 리뷰 수정시 유저안에 리뷰 업데이트하기

--- lib/data/data_source/user_data_source.dart
+++ lib/data/data_source/user_data_source.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../../domain/model/book.dart';
+import '../../domain/model/review.dart';
 import '../../domain/model/user.dart';
 
 class UserDataSource {
@@ -28,5 +30,56 @@ class UserDataSource {
   //Delete
   Future<void> deleteUser({required String userId}) async {
     await _userCollectionRef.doc(userId).delete();
+  }
+
+  Future<void> addReviewToUser(
+      {required String userId, required Review review}) async {
+    await _userCollectionRef.doc(userId).update({
+      'reviewList': FieldValue.arrayUnion([review.toJson()])
+    });
+  }
+
+  Future<void> deleteReviewToUser(
+      {required String userId, required Review review}) async {
+    await _userCollectionRef.doc(userId).update({
+      'reviewList': FieldValue.arrayRemove([review.toJson()])
+    });
+  }
+
+  Future<void> addCurrentReadingBookToUser(
+      {required String userId, required Book book}) async {
+    await _userCollectionRef.doc(userId).update({
+      'currentReadingBookList': FieldValue.arrayUnion([book.toJson()])
+    });
+  }
+
+  Future<void> deleteCurrentReadingBookToUser(
+      {required String userId, required Book book}) async {
+    await _userCollectionRef.doc(userId).update({
+      'currentReadingBookList': FieldValue.arrayRemove([book.toJson()])
+    });
+  }
+
+  Future<void> updateUsersReview(
+      {required String userId,
+      required String reviewContent,
+      required Review review}) async {
+    // 1. 기존의 리뷰 삭제
+    try {
+      await _userCollectionRef.doc(userId).update({
+        'reviewList': FieldValue.arrayRemove([review.toJson()])
+      });
+
+      // 2. 리뷰 객체 업데이트하기
+
+      final updatedReview = review.copyWith(content: reviewContent);
+
+      // 3. 업데이트된 리뷰 저장하기
+      await _userCollectionRef.doc(userId).update({
+        'reviewList': FieldValue.arrayUnion([updatedReview.toJson()])
+      });
+    } catch (e) {
+      throw Exception('updateUserReview fail : $e');
+    }
   }
 }
