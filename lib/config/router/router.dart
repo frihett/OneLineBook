402f7e9de36fae77596ffdab165602b75d7f3193
sseung <sseutta@naver.com>
2024-08-20 23:03:59 +0900

    feat: 여러가지 설정 의존성 추가

--- lib/config/router/router.dart
+++ lib/config/router/router.dart
@@ -1,11 +1,30 @@
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:untitled9/core/provider/user_provider.dart';
+import 'package:untitled9/data/data_source/google_sign_in_data_source.dart';
+import 'package:untitled9/data/data_source/review_data_source.dart';
+import 'package:untitled9/data/data_source/user_data_source.dart';
+import 'package:untitled9/data/data_source/user_stream_data_source.dart';
 import 'package:untitled9/data/repository/book_repository_impl.dart';
+import 'package:untitled9/data/repository/firebase_auth_repository_impl.dart';
+import 'package:untitled9/data/repository/review_repository_impl.dart';
+import 'package:untitled9/data/repository/user_repository_impl.dart';
+import 'package:untitled9/data/repository/user_stream_repository_impl.dart';
+import 'package:untitled9/domain/use_case/add_current_reading_book_use_case.dart';
+import 'package:untitled9/domain/use_case/add_users_review_use_case.dart';
+import 'package:untitled9/domain/use_case/create_review_use_case.dart';
+import 'package:untitled9/domain/use_case/create_user_use_case.dart';
+import 'package:untitled9/domain/use_case/delete_current_reading_book_use_case.dart';
+import 'package:untitled9/domain/use_case/delete_review_to_user_use_case.dart';
+import 'package:untitled9/domain/use_case/edit_review_use_case.dart';
+import 'package:untitled9/domain/use_case/get_review_use_case.dart';
+import 'package:untitled9/domain/use_case/get_user_use_case.dart';
 import 'package:untitled9/presentation/tap/account/account_page.dart';
 import 'package:untitled9/presentation/tap/account/account_page_view_model.dart';
 import 'package:untitled9/presentation/tap/home/home_page.dart';
 import 'package:untitled9/presentation/tap/home/home_page_view_model.dart';
 import 'package:untitled9/presentation/tap/login/login_page.dart';
+import 'package:untitled9/presentation/tap/login/login_page_view_model.dart';
 import 'package:untitled9/presentation/tap/ranking/raking_page.dart';
 import 'package:untitled9/presentation/tap/review_search/review_page.dart';
 import 'package:untitled9/presentation/tap/review_search/review_page_view_model.dart';
@@ -34,15 +53,30 @@ final goRouter = GoRouter(
         GoRoute(
             path: '/home',
             builder: (context, state) {
-              return HomePage(
-                  homePageViewModel:
-                      HomePageViewModel(bookRepository: BookRepositoryImpl()));
+              return ChangeNotifierProvider(
+                create: (context) => HomePageViewModel(
+                    userStreamRepository: UserStreamRepositoryImpl(
+                        userStreamDataSource: UserStreamDataSource()),
+                    deleteCurrentReadingBookUseCase:
+                        DeleteCurrentReadingBookUseCase(
+                            userRepository: UserRepositoryImpl(
+                                userDataSource: UserDataSource()))),
+                child: HomePage(),
+              );
             }),
         GoRoute(
             path: '/reviewSearching',
             builder: (context, state) {
               return ChangeNotifierProvider(
-                  create: (context) => ReviewPageViewModel(),
+                  create: (context) => ReviewPageViewModel(
+                      deleteReviewToUserUseCase: DeleteReviewToUserUseCase(
+                          userRepository: UserRepositoryImpl(
+                              userDataSource: UserDataSource())),
+                      editReviewUseCase: EditReviewUseCase(
+                          userRepository: UserRepositoryImpl(
+                              userDataSource: UserDataSource()),
+                          reviewRepository: ReviewRepositoryImpl(
+                              reviewDataSource: ReviewDataSource()))),
                   child: ReviewPage());
             }),
         GoRoute(
@@ -64,8 +98,11 @@ final goRouter = GoRouter(
         path: '/bookSearching',
         builder: (context, state) {
           return ChangeNotifierProvider(
-              create: (context) =>
-                  SearchPageViewModel(bookRepository: BookRepositoryImpl()),
+              create: (context) => SearchPageViewModel(
+                  bookRepository: BookRepositoryImpl(),
+                  addCurrentReadingBookUseCase: AddCurrentReadingBookUseCase(
+                      userRepository: UserRepositoryImpl(
+                          userDataSource: UserDataSource()))),
               child: SearchPage());
         }),
     // ChangeNotifierProvider(
@@ -76,7 +113,16 @@ final goRouter = GoRouter(
           final book = state.extra as Book?;
 
           return ChangeNotifierProvider(
-              create: (context) => WritingPageViewModel(),
+              create: (context) => WritingPageViewModel(
+                  createReviewUseCase: CreateReviewUseCase(
+                      reviewRepository: ReviewRepositoryImpl(
+                          reviewDataSource: ReviewDataSource())),
+                  addUsersReviewUseCase: AddUsersReviewUseCase(
+                      userRepository:
+                          UserRepositoryImpl(userDataSource: UserDataSource())),
+                  getReviewUseCase: GetReviewUseCase(
+                      reviewRepository: ReviewRepositoryImpl(
+                          reviewDataSource: ReviewDataSource()))),
               child: WritingPage(
                 book: book,
               ));
@@ -85,7 +131,19 @@ final goRouter = GoRouter(
     GoRoute(
         path: '/login',
         builder: (context, state) {
-          return LoginPage();
+          return ChangeNotifierProvider(
+            create: (context) => LoginPageViewModel(
+                firebaseAuthRepository: FirebaseAuthRepositoryImpl(
+                    googleSignInDataSource: GoogleSignInDataSource()),
+                userProvider: UserProvider(
+                    getUserUseCase: GetUserUseCase(
+                        userRepository: UserRepositoryImpl(
+                            userDataSource: UserDataSource())),
+                    createUserUseCase: CreateUserUseCase(
+                        userRepository: UserRepositoryImpl(
+                            userDataSource: UserDataSource())))),
+            child: LoginPage(),
+          );
         }),
   ],
 );
