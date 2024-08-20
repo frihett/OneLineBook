1e639e0173dd9cf5ab12ebcc42f4e61408a5da7e
sseung <sseutta@naver.com>
2024-08-20 23:12:01 +0900

    feat: 여러가지 메소드 작성
    addReviewToUser(), deleteReviewToUser(),
    addCurrentReadingBookToUser(),
    deleteCurrentReadingBookToUser(),
    updateUsersReview() 작성

--- lib/data/repository/user_repository_impl.dart
+++ lib/data/repository/user_repository_impl.dart
@@ -1,8 +1,11 @@
 import 'package:untitled9/data/data_source/user_data_source.dart';
 
+import '../../domain/model/book.dart';
+import '../../domain/model/review.dart';
 import '../../domain/model/user.dart';
+import '../../domain/repository/user_repository.dart';
 
-class UserRepositoryImpl {
+class UserRepositoryImpl implements UserRepository {
   final UserDataSource _userDataSource;
 
   UserRepositoryImpl({required UserDataSource userDataSource})
@@ -38,5 +41,51 @@ class UserRepositoryImpl {
     } catch (e) {
       throw Exception('fail : $e');
     }
+  }
+
+  Future<void> addReviewToUser(
+      {required String userId, required Review review}) async {
+    try {
+      await _userDataSource.addReviewToUser(userId: userId, review: review);
+    } catch (e) {
+      throw Exception('addReviewToUser fail : $e');
+    }
+  }
+
+  Future<void> deleteReviewToUser(
+      {required String userId, required Review review}) async {
+    try {
+      await _userDataSource.deleteReviewToUser(userId: userId, review: review);
+    } catch (e) {
+      throw Exception('addReviewToUser fail : $e');
+    }
+  }
+
+  Future<void> addCurrentReadingBookToUser(
+      {required String userId, required Book book}) async {
+    try {
+      await _userDataSource.addCurrentReadingBookToUser(
+          userId: userId, book: book);
+    } catch (e) {
+      throw Exception('addCurrentReadingBookToUser fail : $e');
+    }
+  }
+
+  Future<void> deleteCurrentReadingBookToUser(
+      {required String userId, required Book book}) async {
+    try {
+      await _userDataSource.deleteCurrentReadingBookToUser(
+          userId: userId, book: book);
+    } catch (e) {
+      throw Exception('deleteCurrentReadingBookToUser fail : $e');
+    }
+  }
+
+  Future<void> updateUsersReview(
+      {required String userId,
+      required String reviewContent,
+      required Review review}) async {
+    await _userDataSource.updateUsersReview(
+        userId: userId, reviewContent: reviewContent, review: review);
   }
 }
--- lib/domain/repository/user_repository.dart
+++ lib/domain/repository/user_repository.dart
@@ -1,3 +1,5 @@
+import '../model/book.dart';
+import '../model/review.dart';
 import '../model/user.dart';
 
 abstract interface class UserRepository {
@@ -8,4 +10,20 @@ abstract interface class UserRepository {
   Future<void> updateUser({required User user});
 
   Future<void> deleteUser({required String userId});
+
+  Future<void> addReviewToUser(
+      {required String userId, required Review review});
+
+  Future<void> deleteReviewToUser(
+      {required String userId, required Review review});
+
+  Future<void> addCurrentReadingBookToUser(
+      {required String userId, required Book book});
+
+  Future<void> deleteCurrentReadingBookToUser(
+      {required String userId, required Book book});
+
+  Future<void> updateUsersReview({required String userId,
+    required String reviewContent,
+    required Review review});
 }
