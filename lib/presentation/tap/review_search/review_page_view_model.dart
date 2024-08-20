4ac4ac40d80aff022c70c6e61ca6d48061a5fb44
sseung <sseutta@naver.com>
2024-08-20 23:01:27 +0900

    feat: deleteReview , editReview 메소드 추가
    deleteReview() - 리뷰삭제
    editReview() - 리뷰 수정

--- lib/presentation/tap/review_search/review_page_view_model.dart
+++ lib/presentation/tap/review_search/review_page_view_model.dart
@@ -1,12 +1,29 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:untitled9/domain/use_case/delete_review_to_user_use_case.dart';
 
-import '../../../domain/model/post.dart';
+import '../../../domain/model/review.dart';
+import '../../../domain/use_case/edit_review_use_case.dart';
 
 class ReviewPageViewModel with ChangeNotifier {
-  final Stream<QuerySnapshot<Post>> postsStream =
-      FirebaseFirestore.instance.collection('posts').withConverter<Post>(
-          fromFirestore: (snapshot, _) => Post.fromJson(snapshot.data()!),
-          toFirestore: (post, _) => post.toJson(),).snapshots();
+  final DeleteReviewToUserUseCase _deleteReviewToUserUseCase;
+  final EditReviewUseCase _editReviewUseCase;
 
+  ReviewPageViewModel({
+    required EditReviewUseCase editReviewUseCase,
+    required DeleteReviewToUserUseCase deleteReviewToUserUseCase,
+  })  : _deleteReviewToUserUseCase = deleteReviewToUserUseCase,
+        _editReviewUseCase = editReviewUseCase;
+
+  Future<void> deleteReview(
+      {required String userId, required Review review}) async {
+    await _deleteReviewToUserUseCase.execute(userId: userId, review: review);
+  }
+
+  Future<void> editReview(
+      {required String userId,
+      required String reviewContent,
+      required Review review}) async {
+    await _editReviewUseCase.execute(
+        userId: userId, reviewContent: reviewContent, review: review);
+  }
 }
