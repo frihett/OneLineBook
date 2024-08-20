74931081b176ae0e35279078fce1a42b14822b6d
sseung <sseutta@naver.com>
2024-08-20 22:00:26 +0900

    refactor: 변수 이름 변경에 의한 수정

--- lib/presentation/tap/account/account_page.dart
+++ lib/presentation/tap/account/account_page.dart
@@ -5,7 +5,6 @@ import 'package:provider/provider.dart';
 
 import '../../../core/provider/user_provider.dart';
 import '../components/my_page_menu_list.dart';
-import 'account_page_view_model.dart';
 
 class AccountPage extends StatefulWidget {
   AccountPage({super.key});
@@ -22,7 +21,9 @@ class _AccountPageState extends State<AccountPage> {
 
   @override
   Widget build(BuildContext context) {
+
     final userProvider = context.watch<UserProvider>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('내 프로필'),
@@ -36,17 +37,18 @@ class _AccountPageState extends State<AccountPage> {
                 Container(
                   width: 100,
                   height: 100,
-                  child: userProvider.user?.imageUrl != null
-                      ? Image.network(userProvider.user!.imageUrl.toString())
+                  child: userProvider.user?.profilePictureUrl != null
+                      ? Image.network(
+                          userProvider.user!.profilePictureUrl.toString())
                       : Icon(Icons.account_circle, size: 100),
                 ),
                 Column(
                   children: [
-                    Text('안녕하세요 ${userProvider.user?.name}님'),
+                    Text('안녕하세요 ${userProvider.user?.userName}님'),
                     SizedBox(
                       height: 12,
                     ),
-                    Text('이메일은 ${userProvider.user?.email ?? '알 수 없음'}')
+                    Text('이메일은 ${userProvider.user?.email ?? '알 수 없음'}'),
                   ],
                 ),
               ],
