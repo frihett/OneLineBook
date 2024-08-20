12ca2665a98c71955e4aa23405faa4005434ac2b
sseung <sseutta@naver.com>
2024-08-20 22:58:13 +0900

    feat: 스트림 추가, 리뷰수정 추가
    User스트림 추가 - 유저 리뷰 뿌리기
    리뷰수정 추가 - 다이얼로그 뿌리기

--- lib/presentation/tap/review_search/review_page.dart
+++ lib/presentation/tap/review_search/review_page.dart
@@ -2,11 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:untitled9/config/ui_style/ui_style.dart';
+import 'package:untitled9/core/provider/user_provider.dart';
 import 'package:untitled9/presentation/tap/review_search/review_page_view_model.dart';
 
-import '../../../domain/model/post.dart';
-
-final List<String> entries = <String>['A', 'B', 'C'];
+import '../../../domain/model/user.dart';
 
 class ReviewPage extends StatefulWidget {
   const ReviewPage({super.key});
@@ -18,46 +17,30 @@ class ReviewPage extends StatefulWidget {
 class _ReviewPageState extends State<ReviewPage> {
   @override
   Widget build(BuildContext context) {
+    final userProvider = context.watch<UserProvider>();
+    final userId = userProvider.user?.userId;
     final model = context.watch<ReviewPageViewModel>();
 
-    return Scaffold(
+    final Stream<DocumentSnapshot<User>> userStream = FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .withConverter(
+          fromFirestore: (snapshot, _) => User.fromJson(snapshot.data()!),
+          toFirestore: (user, _) => user.toJson(),
+        )
+        .snapshots();
 
+    return Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text('리뷰 검색'),
+          title: Text('나의 리뷰'),
         ),
         body: SafeArea(
           child: Column(
             children: [
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    width: 300,
-                    child: TextFormField(
-                      onTap: () {},
-                      decoration: InputDecoration(
-                        enabledBorder: const OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color(0xffD7D7D7),
-                            width: 3,
-                          ),
-                          borderRadius: BorderRadius.all(Radius.circular(50)),
-                        ),
-                        contentPadding: const EdgeInsets.symmetric(vertical: 8),
-                        hintText: '리뷰 검색하기',
-                        prefixIcon: const Padding(
-                          padding: EdgeInsets.only(left: 16.0),
-                          child: Icon(Icons.search),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
               Expanded(
-                child: StreamBuilder<QuerySnapshot<Post>>(
-                    stream: model.postsStream,
+                child: StreamBuilder<DocumentSnapshot<User>>(
+                    stream: userStream,
                     builder: (context, snapshot) {
                       if (snapshot.hasError) {
                         return Text('알 수 없는 에러');
@@ -67,14 +50,15 @@ class _ReviewPageState extends State<ReviewPage> {
                         return Center(child: CircularProgressIndicator());
                       }
 
-                      List<Post> posts =
-                          snapshot.data!.docs.map((e) => e.data()).toList();
-
+                      final user = snapshot.data?.data();
+                      if (user == null || user.reviewList == null) {
+                        return const Text('No user data available');
+                      }
                       return ListView.builder(
                           padding: const EdgeInsets.all(16),
-                          itemCount: posts.length,
+                          itemCount: user.reviewList?.length,
                           itemBuilder: (BuildContext context, int index) {
-                            final post = posts[index];
+                            final review = user.reviewList![index];
                             return Container(
                               margin: EdgeInsets.only(
                                   right: 16, left: 16, bottom: 16, top: 16),
@@ -87,12 +71,12 @@ class _ReviewPageState extends State<ReviewPage> {
                                     width: 2.0,
                                   )),
                               padding: EdgeInsets.all(16),
-                              height: 250,
+                              height: 300,
                               child: Column(
                                 children: [
-                                  post.imageUrl.isEmpty == false
+                                  review.book.bookImageUrl.isEmpty == false
                                       ? Image.network(
-                                          post.imageUrl,
+                                          review.book.bookImageUrl,
                                           height: 130,
                                           fit: BoxFit.cover,
                                         )
@@ -104,7 +88,7 @@ class _ReviewPageState extends State<ReviewPage> {
                                     height: 8,
                                   ),
                                   Text(
-                                    post.title,
+                                    review.book.title,
                                     // style: UiStyle.h1Style,
                                     // style: UiStyle.h2Style,
                                     // style: UiStyle.h3Style,
@@ -117,20 +101,91 @@ class _ReviewPageState extends State<ReviewPage> {
                                     overflow: TextOverflow.ellipsis,
                                   ),
                                   Container(
-
                                     child: Text(
-                                      '한줄평 - ${post.review}',
+                                      '한줄평 - ${review.content}',
                                       style: UiStyle.bodyStyle,
                                     ),
                                     decoration: BoxDecoration(
-
-                                      color:Colors.white,
+                                        color: Colors.white,
                                         border: Border.all(
-                                      color: Colors.white,
-                                    )),
+                                          color: Colors.white,
+                                        )),
                                     width: 400,
-                                    height:49 ,
-                                    padding: EdgeInsets.only(left: 8,right: 8),
+                                    height: 49,
+                                    padding: EdgeInsets.only(left: 8, right: 8),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      IconButton(
+                                        icon: Icon(
+                                          Icons.edit,
+                                        ),
+                                        onPressed: () async {
+                                          // 다이얼로그를 열어서 새로운 리뷰 내용을 입력받음
+                                          final newReviewContent =
+                                              await showDialog<String>(
+                                            context: context,
+                                            builder: (BuildContext context) {
+                                              String content = review
+                                                  .content; // 기존 리뷰 내용을 기본값으로 설정
+                                              return AlertDialog(
+                                                title: Text('리뷰 수정'),
+                                                content: TextField(
+                                                  onChanged: (value) {
+                                                    content =
+                                                        value; // 입력된 내용을 content에 저장
+                                                  },
+                                                  controller:
+                                                      TextEditingController(
+                                                          text: review.content),
+                                                  decoration: InputDecoration(
+                                                    hintText:
+                                                        "새로운 리뷰 내용을 입력하세요",
+                                                  ),
+                                                ),
+                                                actions: <Widget>[
+                                                  TextButton(
+                                                    child: Text('취소'),
+                                                    onPressed: () {
+                                                      Navigator.of(context)
+                                                          .pop(); // 다이얼로그 닫기
+                                                    },
+                                                  ),
+                                                  TextButton(
+                                                    child: Text('저장'),
+                                                    onPressed: () {
+                                                      Navigator.of(context).pop(
+                                                          content); // 입력된 내용을 반환하며 다이얼로그 닫기
+                                                    },
+                                                  ),
+                                                ],
+                                              );
+                                            },
+                                          );
+
+                                          // 사용자가 새로운 내용을 입력하고 저장을 눌렀다면
+                                          if (newReviewContent != null &&
+                                              newReviewContent.isNotEmpty) {
+                                            model.editReview(
+                                              userId: userProvider.getUserId(),
+                                              reviewContent: newReviewContent,
+                                              // 새로 입력된 리뷰 내용 전달
+                                              review: review,
+                                            );
+                                          }
+                                        },
+                                      ),
+                                      IconButton(
+                                        icon: Icon(
+                                          Icons.delete,
+                                        ),
+                                        onPressed: () {
+                                          model.deleteReview(
+                                              userId: userId!, review: review);
+                                        },
+                                      ),
+                                    ],
                                   ),
                                 ],
                               ),
