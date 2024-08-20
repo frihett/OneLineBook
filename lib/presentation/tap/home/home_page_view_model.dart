f5e4826d006a7ffd923757bf54084c44fd0f7ddf
sseung <sseutta@naver.com>
2024-08-20 22:17:17 +0900

    feat: getUserStream(), deleteCurrentReadingBook() 구현
    userStream 가져오기,
    현재 읽고있는 책 삭제하기

--- lib/presentation/tap/home/home_page_view_model.dart
+++ lib/presentation/tap/home/home_page_view_model.dart
@@ -2,32 +2,30 @@ import 'package:flutter/cupertino.dart';
 import 'package:untitled9/domain/repository/book_repository.dart';
 
 import '../../../domain/model/book.dart';
+import '../../../domain/model/user.dart';
+import '../../../domain/repository/user_stream_repository.dart';
+import '../../../domain/use_case/delete_current_reading_book_use_case.dart';
 
 class HomePageViewModel with ChangeNotifier {
-  final BookRepository _bookRepository;
+  final UserStreamRepository _userStreamRepository;
+  final DeleteCurrentReadingBookUseCase _deleteCurrentReadingBookUseCase;
 
-  HomePageViewModel({
-    required BookRepository bookRepository,
-  }) : _bookRepository = bookRepository;
+  HomePageViewModel(
+      {required UserStreamRepository userStreamRepository,
+      required DeleteCurrentReadingBookUseCase deleteCurrentReadingBookUseCase})
+      : _userStreamRepository = userStreamRepository,
+        _deleteCurrentReadingBookUseCase = deleteCurrentReadingBookUseCase;
 
-  List<Book> _book = [];
+  Stream<User?> getUserStream({required String userId}) {
+    return _userStreamRepository.getUserStream(userId: userId);
+  }
 
-  List<Book> get book => List.unmodifiable(_book);
+  Future<void> deleteCurrentReadingBook(
+      {required String userId, required Book book}) async {
+    await _deleteCurrentReadingBookUseCase.execute(userId: userId, book: book);
+  }
 
   bool _isLoading = false;
 
   bool get isLoading => _isLoading;
-
-  void fetchBook(String query) async {
-    _isLoading = true;
-    notifyListeners();
-    try {
-      _book = await _bookRepository.getBookList(query);
-    } catch (e) {
-      print('Error fetching books: $e');
-    } finally {
-      _isLoading = false;
-      notifyListeners();
-    }
-  }
 }
