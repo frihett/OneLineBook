eeb6c9b87251f29c9bdef8c031366bd03249b7fc
sseung <sseutta@naver.com>
2024-08-23 21:52:41 +0900

    feat: allReviewPageViewModel에 toggleLikeReview() 메소드 추가
    따봉 누를시 리뷰객체에 like+1 , 유저아이디+

--- lib/presentation/tap/all_review/all_review_page_view_model.dart
+++ lib/presentation/tap/all_review/all_review_page_view_model.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/widgets.dart';
+import 'package:untitled9/domain/use_case/toggle_like_review_use_case.dart';
+
+import '../../../domain/model/review.dart';
 
 class AllReviewPageViewModel with ChangeNotifier {
+  final ToggleLikeReviewUseCase _toggleLikeReviewUseCase;
 
+  AllReviewPageViewModel({
+    required ToggleLikeReviewUseCase toggleLikeReviewUseCase,
+  }) : _toggleLikeReviewUseCase = toggleLikeReviewUseCase;
 
-
+  Future<void> toggleLikeReview(
+      {required Review review, required String userId}) async {
+    await _toggleLikeReviewUseCase.execute(review: review, userId: userId);
+  }
 }
