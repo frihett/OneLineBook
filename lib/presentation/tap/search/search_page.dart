c5b4d13b4a29f0ee3176b54a17fb839e8587ed6f
sseung <sseutta@naver.com>
2024-08-26 10:02:34 +0900

    feat: searchPage 코드수정
    클릭시 북디테일 화면으로 이동, UI변경, 리뷰쓰러가기, 읽고 있는 책에 추가하기 기능 삭제

--- lib/presentation/tap/search/search_page.dart
+++ lib/presentation/tap/search/search_page.dart
@@ -18,12 +18,13 @@ class _SearchPage extends State<SearchPage> {
   late TextEditingController _controller;
   String query = '';
 
+  @override
   void initState() {
     super.initState();
-
     _controller = TextEditingController();
   }
 
+  @override
   void dispose() {
     _controller.dispose();
     super.dispose();
@@ -34,151 +35,129 @@ class _SearchPage extends State<SearchPage> {
     final model = context.watch<SearchPageViewModel>();
     final userProvider = context.watch<UserProvider>();
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('1 Line Reviewer'),
-        ),
-        body: SafeArea(
-          child: Column(
-            children: [
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    width: 300,
-                    child: TextFormField(
-                      controller: _controller,
-                      onFieldSubmitted: (value) {
-                        setState(() {
-                          query = value;
-                        });
-                        model.fetchBook(query);
-                      },
-                      onTap: () {},
-                      decoration: InputDecoration(
-                        enabledBorder: const OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color(0xffD7D7D7),
-                            width: 3,
-                          ),
-                          borderRadius: BorderRadius.all(Radius.circular(50)),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('1 Line Reviewer'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  width: 300,
+                  child: TextFormField(
+                    controller: _controller,
+                    onFieldSubmitted: (value) {
+                      setState(() {
+                        query = value;
+                      });
+                      model.fetchBook(query);
+                    },
+                    decoration: InputDecoration(
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xffD7D7D7),
+                          width: 3,
                         ),
-                        contentPadding: const EdgeInsets.symmetric(vertical: 8),
-                        hintText: '도서 검색하기',
-                        prefixIcon: const Padding(
-                          padding: EdgeInsets.only(left: 16.0),
-                          child: Icon(Icons.search),
-                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(vertical: 8),
+                      hintText: '도서 검색하기',
+                      prefixIcon: const Padding(
+                        padding: EdgeInsets.only(left: 16.0),
+                        child: Icon(Icons.search),
                       ),
                     ),
                   ),
                 ),
               ),
-              SizedBox(
-                height: 24,
-              ),
-              Expanded(
-                child: GridView.builder(
-                  padding: EdgeInsets.all(16),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 1, // 열의 수
-                    mainAxisSpacing: 10.0, // 수직 간격
-                    crossAxisSpacing: 10.0, // 수평 간격
-                    childAspectRatio: 0.8, // 각 아이템의 가로 세로 비율
-                  ),
-                  itemCount: model.book.length, // 아이템 개수
-                  itemBuilder: (context, index) {
-                    final book = model.book[index];
-                    return GridTile(
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Container(
-                          padding: EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                              color: Colors.blueGrey[100],
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Image.network(
-                                      book.bookImageUrl,
-                                      fit: BoxFit.cover,
-                                      height: 80,
-                                    ),
-                                    SizedBox(
-                                      width: 8,
-                                    ),
-                                    Column(
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: model.book.length, // 아이템 개수
+                itemBuilder: (context, index) {
+                  final book = model.book[index];
+                  return GestureDetector(
+                    onTap: () {
+                      context.push('/book_detail', extra: book);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey[100],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.network(
+                                    book.bookImageUrl,
+                                    fit: BoxFit.cover,
+                                    height: 160,
+                                  ),
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  Expanded(
+                                    child: Column(
                                       mainAxisAlignment:
                                           MainAxisAlignment.start,
                                       crossAxisAlignment:
                                           CrossAxisAlignment.start,
                                       children: [
                                         Text(
-                                            book.title.length < 14
-                                                ? book.title
-                                                : book.title.substring(0, 14),
-                                            style: TextStyle(fontSize: 20),
-                                            maxLines: 1,
-                                            overflow: TextOverflow.ellipsis),
-                                        Text(book.authors.toString().length < 23
-                                            ? book.authors.toString()
-                                            : book.authors
-                                                .toString()
-                                                .substring(0, 23))
+                                          book.title.length < 14
+                                              ? book.title
+                                              : book.title.substring(0, 14),
+                                          style: TextStyle(fontSize: 20),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        Text(
+                                          book.authors.toString().length < 23
+                                              ? book.authors.toString()
+                                              : book.authors
+                                                  .toString()
+                                                  .substring(0, 23),
+                                        ),
+                                        Text(
+                                          book.description.isEmpty
+                                              ? '내용없음'
+                                              : book.description,
+                                          style: TextStyle(fontSize: 20),
+                                          maxLines: 3,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
                                       ],
-                                    )
-                                  ],
-                                ),
-                                Text(DateFormat('yyyy-MM-dd')
-                                    .format(DateTime.now())),
-                                SizedBox(height: 16),
-                                Text(
-                                  book.description.isEmpty
-                                      ? '내용없음'
-                                      : book.description,
-                                  style: TextStyle(fontSize: 20),
-                                  maxLines: 6,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                Spacer(),
-                                Row(
-                                  children: [
-                                    IconButton(
-                                      icon: Icon(Icons.comment),
-                                      onPressed: () {
-                                        context.go('/writing', extra: book);
-                                      },
                                     ),
-                                    SizedBox(
-                                      width: 8,
-                                    ),
-                                    IconButton(
-                                      icon: Icon(Icons.bookmark),
-                                      onPressed: () async{
-                                        model.addCurrentReadingBookList(
-                                            userId: await userProvider.getUserId(),
-                                            book: book);
-                                      },
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
+                                  ),
+                                ],
+                              ),
+
+                            ],
                           ),
                         ),
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  );
+                },
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
