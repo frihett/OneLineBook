1eca9b4864ba260c9e5c1080bd08ed628c979258
sseung <sseutta@naver.com>
2024-08-26 16:51:00 +0900

    feat: 기능 추가
    getUser() 메소드 추가

--- lib/presentation/tap/all_review/raking_page_view_model.dart
+++ lib/presentation/tap/all_review/raking_page_view_model.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/widgets.dart';
+import 'package:untitled9/domain/use_case/get_user_use_case.dart';
 import 'package:untitled9/domain/use_case/toggle_like_review_use_case.dart';
 
 import '../../../domain/model/review.dart';
+import '../../../domain/model/user.dart';
 
 class RakingPageViewModel with ChangeNotifier {
   final ToggleLikeReviewUseCase _toggleLikeReviewUseCase;
+  final GetUserUseCase _getUserUseCase;
 
   RakingPageViewModel({
+    required GetUserUseCase getUserUseCase,
     required ToggleLikeReviewUseCase toggleLikeReviewUseCase,
-  }) : _toggleLikeReviewUseCase = toggleLikeReviewUseCase;
+  })  : _toggleLikeReviewUseCase = toggleLikeReviewUseCase,
+        _getUserUseCase = getUserUseCase;
 
   Future<void> toggleLikeReview(
       {required Review review, required String userId}) async {
     await _toggleLikeReviewUseCase.execute(review: review, userId: userId);
+  }
+
+  Future<User?> getUser({required String userId}) async {
+    return  _getUserUseCase.execute(userId: userId);
   }
 }
