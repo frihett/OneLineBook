b74e6986adda881be8a3ae337455385011cd78a8
sseung <sseutta@naver.com>
2024-08-27 15:59:33 +0900

    feat: getUser() 추가
    setUpUser()에 파이어스토어에 User가 잇는지 확인하기 위한 getUser()추가

--- lib/presentation/tap/login/login_page_view_model.dart
+++ lib/presentation/tap/login/login_page_view_model.dart
@@ -1,19 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
+import 'package:untitled9/data/repository/user_repository_impl.dart';
 import 'package:untitled9/domain/use_case/create_user_use_case.dart';
 
 import '../../../core/provider/user_provider.dart';
 import '../../../domain/model/user.dart';
 import '../../../domain/repository/firebase_auth_repository.dart';
+import '../../../domain/repository/user_repository.dart';
 import '../../../domain/use_case/get_user_use_case.dart';
 
 class LoginPageViewModel extends ChangeNotifier {
   final FirebaseAuthRepository _firebaseAuthRepository;
+  final UserRepository _userRepository;
 
   LoginPageViewModel({
+    required UserRepository userRepository,
     required FirebaseAuthRepository firebaseAuthRepository,
     required UserProvider userProvider,
-  }) : _firebaseAuthRepository = firebaseAuthRepository;
+  })  : _firebaseAuthRepository = firebaseAuthRepository,
+        _userRepository = userRepository;
 
   Future<auth.UserCredential?> signInWithGoogle() async {
     return await _firebaseAuthRepository.signInWithGoogle();
@@ -24,6 +29,11 @@ class LoginPageViewModel extends ChangeNotifier {
   }
 
   Future<User> setUpUser(auth.UserCredential userCredential) async {
+
+    final user = await _userRepository.getUser(userId: userCredential.user!.uid);
+    if(user != null){
+      return user;
+    }
     return User(
         userId: userCredential.user!.uid,
         userName: userCredential.user!.displayName!,
